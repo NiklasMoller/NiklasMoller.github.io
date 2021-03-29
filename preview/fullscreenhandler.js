08af86934f451b9d98d37d8688989099a65f76ca
NiklasMoller <niklasxmoller@gmail.com>
2021-03-29 14:46:21 +0200

    Add new build

--- preview/fullscreenhandler.js
+++ preview/fullscreenhandler.js
@@ -1,12 +1,34 @@
+//Default when game starts running
+var fullscreen = true;
+
+var elem = document.documentElement;
+
 function toggleFullscreen(){
-  if (document.fullscreenElement) {
-    // exitFullscreen is only available on the Document object.
-    document.exitFullscreen();
-  } else {
-    document.documentElement.requestFullscreen();
+
+if(fullscreen){
+  unityInstance.SetFullscreen(0);
+  fullscreen = false;
+}else{
+  unityInstance.SetFullscreen(1);
+    fullscreen = true;
+}
+
+}
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
   }
 }
 
 document.documentElement.addEventListener('fullscreenchange', (event) => {
-      unityInstance.SendMessage("FullscreenButton", "updateImageOnExternalFullscreen");
+
+  if(buttonIsPressed){
+    unityInstance.SendMessage("FullscreenButton", "updateImageOnExternalFullscreen");
+  }
+
   });
