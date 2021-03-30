cf8447eadaeb3eab82fd09ec96a748d7418a4366
NiklasMoller <niklasxmoller@gmail.com>
2021-03-30 18:19:35 +0200

    Add new index with jquery

--- jquery/resizehandler.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("resize", onResize);
-
-function onResize(){
-  unityInstance.SendMessage("WindowSizeHandler", "SetWidth", window.innerWidth);
-  unityInstance.SendMessage("WindowSizeHandler", "SetHeight", window.innerHeight);
-  unityInstance.SendMessage("WindowSizeHandler", "OnResize");
-}
