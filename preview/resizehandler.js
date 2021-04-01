a642b99a147c8a1017718251773819045e279676
NiklasMoller <niklasxmoller@gmail.com>
2021-04-01 19:09:02 +0200

    Refactor code

--- preview/resizehandler.js
+++ preview/resizehandler.js
@@ -2,13 +2,12 @@ window.addEventListener("resize", onResize);
 
 function onResize(){
 
-  $(document).ready(function(){
-    
-    unityInstance.SendMessage("WindowResizeHandler", "SetWidth", window.innerWidth);
-    unityInstance.SendMessage("WindowResizeHandler", "SetHeight", window.innerHeight);
-    unityInstance.SendMessage("WindowResizeHandler", "OnResize");
-  
-  });
+if(window.gameIsReady){
 
+  unityInstance.SendMessage("WindowResizeHandler", "SetWidth", window.innerWidth);
+  unityInstance.SendMessage("WindowResizeHandler", "SetHeight", window.innerHeight);
+  unityInstance.SendMessage("WindowResizeHandler", "OnResize");
+
+}
 
 }
