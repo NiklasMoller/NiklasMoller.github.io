6abb0b308c4d38ca3840d1a8ff4ca491749d516e
NiklasMoller <niklasxmoller@gmail.com>
2021-04-01 10:56:20 +0200

    Add SHARE function

---
+++ SHARE/shareHandler.js
@@ -0,0 +1,42 @@
+function donate(){
+/*
+  $(document).ready(function(){
+  
+      $('a[href=https://paypal.me/pools/c/8ycsF13cHY]').click(function(){
+        window.open(this.href);
+        return false;
+      });
+    });
+*/
+    window.open(
+      'href=https://paypal.me/pools/c/8ycsF13cHY',
+      '_blank' // <- This is what makes it open in a new window.
+    );
+  
+  }
+  
+  function shareOnFB(){
+  
+  
+  $(document).ready(function(){
+  
+      $('a[href=https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.anewtreegrows.online%2F&amp;src=sdkpreparse]').click(function(){
+        window.open(this.href);
+        return false;
+      });
+    });
+  
+  }
+  
+  function shareOnTwitter(){
+  
+  
+  $(document).ready(function(){
+  
+      $('a[href=http://www.google.com]').click(function(){
+        window.open(this.href);
+        return false;
+      });
+    });
+  
+  }
